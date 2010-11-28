eef7bd06d19e946ece02ce42d87df3afabff3c8b
Shailesh Sawant <shailesh.sawant@students.sjsu.edu>
2010-11-28 11:41:15 -0800

    push

--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -1,30 +1,31 @@
 class AlbumController < ApplicationController
 
 def new
-@album = Album.new
-respond_with @album
+	# check wheter user is logged in or not
+	@album = Album.new
 end
 
 def create
- @album = Album.create(params[:album])
- @album.save
+	@album = Album.create(params[:album])
+	#find whether the album name and owner sane is in the album table or not....... if not insert it otherwise display error msg that album name is exists for the current user
+	@album.save
+	#at the same time create a directory under that username/albumname physically 
 end
 
-
 def update
-@album = Album.find(params[:id])
-      begin
-         Album.transaction do
-            @album.update_attributes(params[:album])
-         end
-      rescue ActiveRecord::RecordInvalid => e
-         puts "ERROR: we need to let the user know!"
-         return
-      end
+	@album = Album.find(params[:id])
+	
+	begin
+	Album.transaction do
+	@album.update_attributes(params[:album])
+	end
 
-      redirect_to @album
+	rescue ActiveRecord::RecordInvalid => e
+	puts "ERROR: we need to let the user know!"
+	return
+end
 
-
+redirect_to @album
 end
 
 def delete
